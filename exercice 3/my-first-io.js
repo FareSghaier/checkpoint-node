ba67e89f93be393bdcb831a35729cd5c416dbaf6
FareSghaier <sghaier0311@gmail.com>
2020-08-14 10:14:28 +0100

    first commit

---
+++ exercice 3/my-first-io.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const { CLIENT_RENEG_LIMIT } = require('tls');
+ const Buffer = fs.readFileSync(process.argv[2]);
+ const str = Buffer.toString();
+ const lines = str.split('\n').length -1 ;
+ console.log(lines);
---
+++ exercice 4/my-first-async-io.js
@@ -0,0 +1,5 @@
+const { CLIENT_RENEG_LIMIT } = require('tls');
+ const Buffer = fs.readFile(process.argv[2]);
+ const str = Buffer.toString();
+ const lines = str.split('\n').length -1 ;
+ console.log(lines);
