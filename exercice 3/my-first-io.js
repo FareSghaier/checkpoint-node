84c75fbc3424f28712323b554e6291456afd21bb
FareSghaier <sghaier0311@gmail.com>
2020-08-14 10:30:44 +0100

    modification

--- exercice 3/my-first-io.js
+++ exercice 3/my-first-io.js
@@ -1,6 +1,4 @@
 const fs = require('fs');
-const { CLIENT_RENEG_LIMIT } = require('tls');
  const Buffer = fs.readFileSync(process.argv[2]);
  const str = Buffer.toString();
- const lines = str.split('\n').length -1 ;
- console.log(lines);
+ const lines = str.split('\n').length -1;
--- exercice 4/my-first-async-io.js
+++ exercice 4/my-first-async-io.js
@@ -1,6 +1,7 @@
-const { CLIENT_RENEG_LIMIT } = require('tls');
- const Buffer = fs.readFile(process.argv[2]);
- const str = Buffer.toString();
- const lines = str.split('\n').length -1 ;
- console.log(lines);
- console.log("add")
+const fs = require('fs');
+  fs.readFile(process.argv[2],function(err,contents){
+    const str = contents.toString();
+    const lines = str.split('\n').length -1 ;
+    console.log(lines);
+ });
+
